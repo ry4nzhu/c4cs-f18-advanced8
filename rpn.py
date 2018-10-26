aed07de723f29ee546bde0c18fb28924313dc8e5
ryanzhu <ryanzhu@umich.edu>
2018-10-26 15:06:33 -0400

    sub

--- rpn.py
+++ rpn.py
@@ -11,7 +11,10 @@ def calculate(arg):
         except ValueError:
             val1 = stack.pop()
             val2 = stack.pop()
-            result = val1 + val2
+            if token == '+':
+                result = val1 + val2
+            elif token == '-':
+                result = val2 - val1
 
             stack.append(result)
     
--- test_rpn.py
+++ test_rpn.py
@@ -2,7 +2,9 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-        def test_add(self):
-            result = rpn.calculate("1 1 +")
-            self.assertEqual(2,result)
-
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2,result)
+    def test_sub(self):
+        result = rpn.calculate("2 3 -")
+        self.assertEqual(-1,result)
