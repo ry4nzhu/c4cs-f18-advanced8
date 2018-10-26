42c9e8a23cc42ce3cf4045c5ee601448ef0166bc
ryanzhu <ryanzhu@umich.edu>
2018-10-26 15:15:46 -0400

    failed test

--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,6 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("2 3 -")
         self.assertEqual(-1,result)
+    def test_toomany(self):
+        with self.assertRaises(ValueError):
+            result = rpn.calculate("1 2 3 +")
