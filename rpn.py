1b121a41c7f1b5e10466291161ebfe5528523ec3
ryanzhu <ryanzhu@umich.edu>
2018-10-26 15:01:22 -0400

    Addition

--- rpn.py
+++ rpn.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env  python3
 
 def calculate(arg):
-    pass
+    stack = []
+
+    tokens = arg.split()
+
+    for token in tokens:
+        try:
+            stack.append(int(token));
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+
+            stack.append(result)
+    
+    return stack[0]
 
 def main():
     while True:
         calculate(input("rpn calc> "))
+        print(result)
 
 if __name__ == '__main__':
     main()
